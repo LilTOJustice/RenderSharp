8a6157d9586918a973b689738bcef78e6590078c
Tarodane <tyler.b2k@gmail.com>
2023-10-05 23:29:18 -0700

    Implemented Texture Class

---
+++ RendererCommon/Texture.cs
@@ -0,0 +1,83 @@
+﻿using ImageMagick;
+using RenderSharp.Math;
+using RenderSharp.Renderer.Color;
+
+
+
+namespace RendererCommon
+{
+    public class Texture
+    {
+
+        //Fields
+        private RGBA[,] _texture; //This is the color+alpha of pixel [i,j]
+
+        //Properties
+        public Vec2 Size { get; set; } //Unsigned vect
+
+        public int Height { get { return Size.Y; } }
+        public int Width { get { return Size.X; } }
+        
+
+
+
+        public Texture(Vec2 size)
+        {
+            Size = size;
+            _texture = new RGBA[Height, Width];
+        }
+
+        public Texture(Vec2 size, RGBA color)
+        {
+            Size = size;
+            _texture = new RGBA[Height, Width];
+
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    _texture[i, j] = color;
+                }
+            }
+        }
+
+
+        public Texture(string filename)
+        {
+
+            var image = new MagickImage(filename);
+
+            var bmp = image.ToByteArray(MagickFormat.Rgba);
+
+            var channels = image.ChannelCount;
+
+            Size = new Vec2 (image.Width, image.Height); //X-Y so we swap the order
+
+            _texture = new RGBA[Height, Width];
+
+            for (int i = 0; i < image.Height; i++)
+            {
+                int heightStep = i * Width * 4;
+
+                for (int j = 0; j < image.Width; j++)
+                {
+                    int widthStep = heightStep + j * 4;
+
+                    _texture[i, j] = new RGBA
+                        (
+                            bmp[widthStep],
+                            bmp[widthStep + 1],
+                            bmp[widthStep + 2],
+                            bmp[widthStep + 3]
+                        );
+                }
+            }
+        }
+
+        //Indexer
+        public RGBA this[int x, int y]
+        {
+            get { return _texture[y, x]; }
+        }
+    }
+}
