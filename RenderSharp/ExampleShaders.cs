6b391d03af75d3abd907e187da4fe1944018bf7d
LilTOJustice <muianick4@gmail.com>
2023-10-09 00:34:12 -0700

    Introduce FragShader delegate type

---
+++ RenderSharp/ExampleShaders.cs
@@ -0,0 +1,14 @@
+﻿using RenderSharp.Math;
+using RenderSharp.RendererCommon;
+
+namespace RenderSharp
+{
+    public class ExampleShaders
+    {
+
+        FragShader frag = (in RGBA fragIn, out RGBA fragOut, in Vec2 fragCoord, in Vec2 res, double time) =>
+        {
+            fragOut = new RGBA();
+        };
+    }
+}
---
+++ RendererCommon/FragShader.cs
@@ -0,0 +1,6 @@
+﻿using RenderSharp.Math;
+
+namespace RenderSharp.RendererCommon
+{
+    public delegate void FragShader(in RGBA fragIn, out RGBA fragOut, in Vec2 fragCoord, in Vec2 res, double time);
+}
