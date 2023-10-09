9677d8ff9ebefe8a39e3c0dd58423062c89dbec6
LilTOJustice <muianick4@gmail.com>
2023-10-09 01:26:58 -0700

    Introduced FragShaderArgs for easier frag shader writing

--- RendererCommon/FragShader.cs
+++ RendererCommon/FragShader.cs
@@ -2,5 +2,23 @@
 
 namespace RenderSharp.RendererCommon
 {
-    public delegate void FragShader(in RGBA fragIn, out RGBA fragOut, in Vec2 fragCoord, in Vec2 res, double time);
+    public class FragShaderArgs
+    {
+        public readonly RGBA fragIn;
+        public RGBA fragOut;
+        public readonly Vec2 fragCoord;
+        public readonly Vec2 res;
+        public double time;
+
+        public FragShaderArgs(RGBA fragIn, RGBA fragOut, Vec2 fragCoord, Vec2 res, double time)
+        {
+            this.fragIn = fragIn;
+            this.fragOut = fragOut;
+            this.fragCoord = fragCoord;
+            this.res = res;
+            this.time = time;
+        }
+    }
+
+    public delegate void FragShader(FragShaderArgs args);
 }
