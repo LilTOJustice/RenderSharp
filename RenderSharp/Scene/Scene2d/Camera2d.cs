ed94043490d700c568de13ec7462ace62c5017db
LilTOJustice <muianick4@gmail.com>
2024-01-20 00:31:41 -0800

    Decoupled Actor and Camera from Scene2d

---
+++ RenderSharp/Scene/Scene2d/Camera2d.cs
@@ -0,0 +1,38 @@
+﻿using MathSharp;
+
+namespace RenderSharp.Scene
+{
+    /// <summary>
+    /// Virtual camera for viewing the scene.
+    /// </summary>
+    public class Camera2d
+    {
+        /// <summary>
+        /// World location of the camera.
+        /// </summary>
+        public Vec2 Center { get; set; }
+
+        /// <summary>
+        /// Zoom of the camera.
+        /// </summary>
+        public double Zoom { get; set; }
+
+        /// <summary>
+        /// Rotation of the camera in radians.
+        /// </summary>
+        public double Rotation { get; set; }
+
+        /// <summary>
+        /// Constructs a camera.
+        /// </summary>
+        /// <param name="center">Center of the camera in world space.</param>
+        /// <param name="zoom">Zoom of the camera.</param>
+        /// <param name="rotation">Rotation of the camera in radians.</param>
+        public Camera2d(Vec2 center, double zoom, double rotation)
+        {
+            Center = center;
+            Zoom = zoom;
+            Rotation = rotation;
+        }
+    }
+}
--- RenderSharpExample/Program.cs
+++ RenderSharpExample/Program.cs
@@ -11,7 +11,7 @@ namespace RenderSharp
         {
             Scene2d scene = new(60, 6, bgColor: new RGB(0, 0, 255));
             Renderer2d renderer = new(400, 400, scene);
-            Scene2d.Actor actor = new(new FVec2(10, 10));
+            Actor2d actor = new(new FVec2(10, 10));
             scene.AddActor(actor, "Box");
             actor.Shader += ExampleShaders.Ghostly;
             scene.ThinkFunc += (Scene2dInstance scene, double time, double dt) =>
