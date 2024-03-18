08d811df66f5fe6a822007e5669a3becd01bedb4
LilTOJustice <muianick4@gmail.com>
2024-03-17 23:32:29 -0700

    Implemented sphere rendering

---
+++ RenderSharp/Render3d/Scene3d/Builders/SphereActorBuilder.cs
@@ -0,0 +1,21 @@
+﻿using MathSharp;
+
+namespace RenderSharp.Render3d
+{
+    /// <summary>
+    /// Builder for the <see cref="TriangleActor"/> class.
+    /// Used for <see cref="SceneBuilder.FinalStep.WithActor(string, ActorBuilder)"/> within <see cref="SceneBuilder"/>.
+    /// </summary>
+    public class SphereActorBuilder : ActorBuilder
+    {
+        internal override Actor Build()
+        {
+            boundingBoxSize ??= new FVec3(1, 1, 1);
+            rotation ??= new RVec3();
+            texture ??= new Texture(1, 1, color);
+            position ??= new FVec3();
+            fragShader ??= ((FRGBA fragIn, out FRGBA fragOut, Vec2 fragCoord, Vec2 res, double time) => { fragOut = fragIn; });
+            return new SphereActor((FVec3)boundingBoxSize, (RVec3)rotation, (FVec3)position, texture, fragShader);
+        }
+    }
+}
