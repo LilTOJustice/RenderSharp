79f29b7417414dcd6be15335ff866459bf9ec1a7
LilTOJustice <muianick4@gmail.com>
2024-03-21 22:30:16 -0700

    Adjusted example

--- RenderSharpExample/Program.cs
+++ RenderSharpExample/Program.cs
@@ -17,26 +17,13 @@ namespace RenderSharpExample
                 .MakeDynamic()
                 .WithFramerate(framerate)
                 .WithDuration(duration)
-                .WithCamera("main",
-                new FVec3(0, 0, 0),
-                new DVec3(0, 0, 0),
-                fov: new DVec2(90, 90))
-                //.WithActor("sphere", new SphereActorBuilder()
-                //    .WithPosition(new FVec3(0, 0, 5))
-                //    .WithBoundingBoxSize(new FVec3(2, 1, 1))
-                //)
                 .WithActor("box", new BoxActorBuilder()
                    .WithPosition(new FVec3(0, 0, 5))
                    .WithBoundingBoxSize(new FVec3(2, 1, 1))
                 )
                 .WithThink((SceneInstance scene, double time, double dt) =>
                 {
-                    //scene["sphere"].Rotation += new RVec3(0, 5 * dt, 0);
-                    //scene["sphere"].Position += new FVec3(0, 0, -1 * dt);
-                    //scene.Camera.Rotation += new RVec3(0, 0, 1 * dt);
-                    //scene.Camera.Position += new FVec3(0, 0, -1 * dt);
                     scene["box"].Rotation += new RVec3(1.5 * dt, 1 * dt, 2 * dt);
-                    //scene["box"].Position += new FVec3(0, 0, -1 * dt);
                 })
                 .Build();
 
