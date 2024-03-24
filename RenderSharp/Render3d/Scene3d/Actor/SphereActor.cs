622102bd83dd003ebda2f0b117d25c09626066ee
nmuia <nmuia@ics.uci.edu>
2024-03-23 18:38:35 -0700

    Implemented sphere texture projection

--- RenderSharp/Render3d/Scene3d/Actor/SphereActor.cs
+++ RenderSharp/Render3d/Scene3d/Actor/SphereActor.cs
@@ -24,7 +24,17 @@ namespace RenderSharp.Render3d
         {
             if (sphere.Intersects(worldVec, cameraPos, minDepth, out depth))
             {
-                sample = new RGBA(255, 0, 0, 255);
+                FVec3 fromCenter = (worldVec * depth - (Position - cameraPos)).Rotate(Rotation);
+                double theta1 = fromCenter.X == 0 ?
+                    (fromCenter.Y < 0 ? -Math.PI / 2 : Math.PI / 2)
+                    : Math.Atan(fromCenter.Y / fromCenter.X);
+                double theta2 = fromCenter.X == 0 ?
+                    (fromCenter.Z < 0 ? -Math.PI / 2 : Math.PI / 2)
+                    : Math.Atan(fromCenter.Z / fromCenter.X);
+                FVec2 uv = new FVec2(
+                    (theta1 + Math.PI / 2) / Math.PI,
+                    (theta2 + Math.PI / 2) / Math.PI);
+                sample = Texture[uv];
                 return true;
             }
 
